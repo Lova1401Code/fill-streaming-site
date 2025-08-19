bf66eee5fb1d7996f4d42da034aafddeddc856e7
Lova1401Code <lova.ramiharisoa@gmail.com>
2025-08-19 16:43:12 +0300

    update data function created

---
+++ utils/InsertInDatabase.js
@@ -0,0 +1,27 @@
+export default async function insertInDatabase(data,imdbId, type) {
+    if (type === "movie") {
+        try {
+            await prisma.movie.update({
+                where: {
+                    imdbId: imdbId,
+                },
+                data: data,
+            });
+        } catch (error) {
+            console.error(error);
+            return null;
+        }   
+    } else if (type === "serie") {
+        try {
+            await prisma.series.update({
+                where: {
+                    imdbId: imdbId,
+                },
+                data: data,
+            });
+        } catch (error) {
+            console.error(error);
+            return null;
+        }
+    }
+}
